e401c57731715f550de50f5074dc37bba6cdf0a9
KenCz <ckenrx94@gmail.com>
2021-06-07 00:09:24 -0600

    Crear VMCheque.

---
+++ Banpro/ViewModels/VMCheques.cs
@@ -0,0 +1,20 @@
+﻿using Banpro.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banpro.ViewModels
+{
+    public class VMCheques
+    {
+
+        [Required]
+        [Display(Name = "Archivo")]
+        public List<IFormFile> FormFiles { get; set; }
+        public List<ImagenCheque> Imagenes { get; set; }
+        public ImagenCheque Imagen { get; set; }
+    }
+}
