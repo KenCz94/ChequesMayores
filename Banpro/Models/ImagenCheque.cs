4ae76a2aa0e261fa70ab74f8d9c4fa831a0f5319
KenCz <ckenrx94@gmail.com>
2021-06-07 00:09:02 -0600

    Crear modelo para ImagenCheque.

---
+++ Banpro/Models/ImagenCheque.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banpro.Models
+{
+    public class ImagenCheque
+    {
+        public string NombreFrenteCompleto { get; set; }
+        public string NombreFrente { get; set; }
+        public string NombreReverso { get; set; }
+        public string Ruta { get; set; }
+        public string Cuenta { get; set; }
+        public string NumCheque { get; set; }
+        public string Moneda { get; set; }
+        public decimal Monto { get; set; }
+
+    }
+}
